32b753a6535026af5cb51cbc241038d41a19d609
shahinvaziri <shahinnvaziri@gmail.com>
2024-02-05 18:08:17 +0330

    add products and pictures to  html page

---
+++ .history/digikala/shop/views_20240205115932.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+ from .models import Product
+
+# Create your views here.
+
+
+def helloworld(request):
+    # return HttpResponse('<h1>به فروشگاه من خوش آمدید<h1/>')
+    return render(request, 'index.html')
---
+++ .history/digikala/shop/views_20240205120349.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+ from .models import Product
+
+# Create your views here.
+
+
+def helloworld(request):
+    all_Products = Product.objects.all()
+    return HttpResponse ()
+    # return HttpResponse('<h1>به فروشگاه من خوش آمدید<h1/>')
+    # return render(request, 'index.html')
